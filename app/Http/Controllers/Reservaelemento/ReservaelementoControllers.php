f0f3aac8af3e7ab0930be7a25d543296cf394d9f
Samuel Cardenas <samuelcard2023@gmail.com>
2025-09-05 09:36:16 -0500

    BienestarMind + jetstream

---
+++ app/Http/Controllers/Reservaelemento/ReservaelementoControllers.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Reservaelemento;
+
+use App\Http\Controllers\Controller;
+use App\Models\Reservaelemento;
+use Illuminate\Http\Request;
+
+class ReservaelementoControllers extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reservaelemento $reservaelemento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reservaelemento $reservaelemento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Reservaelemento $reservaelemento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reservaelemento $reservaelemento)
+    {
+        //
+    }
+}
